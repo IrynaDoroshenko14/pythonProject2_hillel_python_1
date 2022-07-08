eee2bbb9c0d8856b3a3a4e355e0c03b1a0c91b4b
IrynaDoroshenko14 <iodoroshenko@gmail.com>
2022-07-08 15:41:27 +0300

    Added solution for lesson 1

---
+++ hw1/1.py
@@ -0,0 +1,51 @@
+# 1 Створіть дві змінні first=10, second=30.
+# Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+
+first = 10
+second = 30
+
+sum_ = first + second
+print(sum_)
+
+difference = first - second
+print(difference)
+
+multiplication = first * second
+print(multiplication)
+
+division = first / second
+print(division)
+
+floor_dif = first // second
+print(floor_dif)
+
+power = first ** second
+print(power)
+
+modulus = first % second
+print(modulus)
+
+
+# 2 Створіть змінну і запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1.
+# Виведіть на екран результат кожного порівняння.
+
+comparison = first < second
+print(comparison)
+
+comparison = first > second
+print(comparison)
+
+comparison = first == second
+print(comparison)
+
+comparison = first != second
+print(comparison)
+
+
+# 3 Створіть змінну - результат конкатенації (складання) строк str1="Hello " и str2="world".
+# Виведіть на екран.
+
+str_1 = "Hello "
+str_2 = "world!"
+result_text = str_1 + str_2
+print(result_text)
